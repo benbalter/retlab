a1fdf745092d3b381dd8c2fe3dfe2c01a0eb7ce6
Ben Balter <ben.balter@github.com>
2019-04-30 13:42:12 -0400

    fix anchor.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,7 @@ Turbolinks.start();
 
 var anchorJS = require('anchor-js');
 var anchors = new anchorJS();
-anchors.add('h3, h4, h5, h6');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  anchors.add('h3, h4, h5, h6');
+});
