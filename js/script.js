97bcbe9ac8f336c39ae1965358566db801ae2890
BalterBot <ben@balter.com>
2019-06-28 09:58:20 -0400

    fix js error on index

--- js/script.js
+++ js/script.js
@@ -6,30 +6,32 @@ import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import {faKeybase} from '@fortawesome/free-brands-svg-icons/faKeybase';
 import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
 
-var Turbolinks = require('turbolinks');
-
-Turbolinks.start();
-
 var anchorJS = require('anchor-js');
 
 var anchors = new anchorJS();
 
+var Turbolinks = require('turbolinks');
+
+Turbolinks.start();
+
 document.addEventListener('turbolinks:load', function () {
   anchors.add();
 
-  if (typeof DISQUS === 'undefined') {
-    (function () {
-      var d = document; var
-        s = d.createElement('script');
-      s.src = 'https://' + window.disqus_shortname + '.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', Number(new Date()));
-      (d.head || d.body).appendChild(s);
-    })();
-  } else {
-    DISQUS.reset({
-      reload: true,
-      config: window.disqus_config
-    });
+  if (document.getElementById('disqus_thread')) {
+    if (typeof DISQUS === 'undefined') {
+      (function () {
+        var d = document;
+        var s = d.createElement('script');
+        s.src = 'https://' + window.disqus_shortname + '.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', Number(new Date()));
+        (d.head || d.body).appendChild(s);
+      })();
+    } else {
+      DISQUS.reset({
+        reload: true,
+        config: window.disqus_config
+      });
+    }
   }
 });
 
