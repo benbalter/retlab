5a8f63860934ab8bbc31a3030bf2bb0118a8a52a
Ben Balter <ben@balter.com>
2021-06-15 20:18:51 -0400

    update all the things

--- js/script.ts
+++ js/script.ts
@@ -1,50 +1,21 @@
-import {library, dom} from '@fortawesome/fontawesome-svg-core';
-import {faRss} from '@fortawesome/free-solid-svg-icons/faRss';
-import {faFacebookF} from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import {faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter';
-import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import {faKeybase} from '@fortawesome/free-brands-svg-icons/faKeybase';
-import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
-import 'bootstrap/js/dist/collapse'
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faRss } from '@fortawesome/free-solid-svg-icons/faRss';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faKeybase } from '@fortawesome/free-brands-svg-icons/faKeybase';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import * as AnchorJS from 'anchor-js';
+import * as Collapse from 'bootstrap/js/dist/collapse';
+import * as Turbo from '@hotwired/turbo';
 
-const anchorJS = require('anchor-js');
+window.Turbo = Turbo;
+window.Collapse = Collapse;
 
-const anchors = new anchorJS();
+const anchors = new AnchorJS();
 
-const Turbolinks = require('turbolinks');
-
-Turbolinks.start();
-
-declare global {
-    interface Window {
-      disqus_config: {};
-      disqus_shortname: string;
-    }
-    let DISQUS: any;
-}
-
-window.disqus_config = window.disqus_config || {};
-window.disqus_shortname = window.disqus_shortname || "";
-
-document.addEventListener('turbolinks:load', function () {
+document.addEventListener('turbo:load', () => {
   anchors.add();
-
-  if (document.getElementById('disqus_thread')) {
-    if (typeof DISQUS === 'undefined') {
-      (function () {
-        const d = document;
-        const s = d.createElement('script');
-        s.src = 'https://' + window.disqus_shortname + '.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', Number(new Date()).toString());
-        (d.head || d.body).appendChild(s);
-      })();
-    } else {
-      DISQUS.reset({
-        reload: true,
-        config: window.disqus_config,
-      });
-    }
-  }
 });
 
 library.add(faRss);
