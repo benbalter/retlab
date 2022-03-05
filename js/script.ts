91fe8c368b284adb9832baca953a666c39e9fb6c
Ben Balter <ben@balter.com>
2022-03-05 13:10:50 -0500

    add tooltip

--- js/script.ts
+++ js/script.ts
@@ -5,17 +5,23 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faKeybase } from '@fortawesome/free-brands-svg-icons/faKeybase';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import Collapse from 'bootstrap/js/dist/collapse';
+import { Collapse, Tooltip } from 'bootstrap';
+
 import * as AnchorJS from 'anchor-js';
 import * as Turbo from '@hotwired/turbo';
 
 window.Turbo = Turbo;
 window.Collapse = Collapse;
-
+window.Tooltip = Tooltip;
 const anchors = new AnchorJS();
 
 document.addEventListener('turbo:load', () => {
   anchors.add();
+
+  const els = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  Array.from(els).forEach((el) => {
+    new Tooltip(el); // eslint-disable-line no-new
+  });
 });
 
 library.add(faRss);
