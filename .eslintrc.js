f9c017a7a590002cb1dd6310b80d80edf72689ed
Ben Balter <ben@balter.com>
2022-08-15 18:57:35 -0400

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   rules: {
   },
-};
+}
