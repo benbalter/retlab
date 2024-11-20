ce3e9f58d272e4375c444b91fab0859374741438
Ben Balter <ben@balter.com>
2024-11-20 18:27:38 -0500

    add bluesky icon

--- js/script.ts
+++ js/script.ts
@@ -6,6 +6,7 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons/faAddressCard'
+import { faBluesky } from '@fortawesome/free-brands-svg-icons/faBluesky'
 import { faClock } from '@fortawesome/free-regular-svg-icons/faClock'
 import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart'
 import { closest } from 'fastest-levenshtein'
@@ -58,6 +59,7 @@ library.add(
   faAddressCard,
   faRetweet,
   faHeart,
-  faClock
+  faClock,
+  faBluesky
 )
 dom.watch()
